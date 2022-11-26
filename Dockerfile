90cb005d3d30b88fe69b8ba78849b1fc38c91902
Vinod Ushake <vinod.ushake@gmail.com>
2022-11-26 21:21:50 +1100

    inital commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+##imported Docker images from Hub.docker.com
+FROM python:3.9.15-slim-bullseye
+ENV PORT 8080
+WORKDIR /ment360docker
+COPY . /ment360docker
+RUN pip install -r ment360requirement.txt
+EXPOSE $PORT
+CMD ["python", "flask_app.py"]
---
+++ flask_app.py
@@ -0,0 +1,17 @@
+
+from flask import Flask
+import os
+PORT = os.environ.get("PORT",5000)
+
+server = Flask(__name__)
+
+@server.route("/home",methods=["GET"])
+def home():
+    return {"message":"Hello world!"}
+
+@server.route("/",methods=["GET"])
+def root():
+    return {"message":"You are on root page"}
+
+# if __name__ == "__main__" :
+server.run("0.0.0.0",PORT)
