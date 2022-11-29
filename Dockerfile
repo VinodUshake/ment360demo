a819aebeab297a4be0963e87f1f4812c900a4a30
Vinod Ushake <vinod.ushake@gmail.com>
2022-11-29 20:04:15 +1100

    changing flask server

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
-##Running required programs through docker file
+##Cloud Run serverless service vm, intall docker file (container run source code by dependece installation).
+##its run all service with out compute resources
+##docker file -->--> process --> doc file --> wok dir -->copy -->run --> txt req -->port externally
+##Running program required program through docker file
 ##imported Docker images from Hub.docker.com
+#sytax
 FROM python:3.9.15-slim-bullseye
 ENV PORT 8080
 WORKDIR /ment360docker
--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,8 @@
-##requirement python run dependence & making server ready
+##requirements python run dependence & making server is  ready
+##used Flash requried for  Frame Works to implement  Urls & import
+##OS Handshake Window b/w Pycharm
+##Port will set URl HTTP from OS get CMD 5000 to use
+
 from flask import Flask
 import os
 PORT = os.environ.get("PORT",5000)
@@ -9,13 +13,15 @@ server = Flask(__name__)
 def home():
     return {"message":"Hello world!"}
 @server.route("/contact",methods=["GET"])
+
+# 1st step displaying after URL's generation main root page
 def contact():
     return {"message":"Welcome Ment360!"}
 
 #Main URL Page
 @server.route("/",methods=["GET"])
 def root():
-    return {"message":"Demo Test"}
+    return {"message":"Rocks"}
 
-#Wellcome
+#if click on this port it will run this code
 server.run("0.0.0.0",PORT)
